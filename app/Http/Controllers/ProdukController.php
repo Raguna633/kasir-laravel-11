f57d17673e7867540ce87f13ccd7b6db77a6baee
Raguna633 <88103737+Raguna633@users.noreply.github.com>
2025-05-31 12:49:56 +0700

    mmmeehhh

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -82,58 +82,6 @@ class ProdukController extends Controller
     }
 
 
-    // public function store(Request $request)
-    // {
-
-    //     $produk = Produk::latest()->first() ?? new Produk();
-    //     if ($request['kode_produk'] == null) {
-    //         $request['kode_produk'] = tambah_nol_didepan((int)$produk->id_produk + 1, 6);
-    //     };
-
-    //     $request->validate([
-    //         'kode_produk' => 'required|unique:produk,kode_produk',
-    //         'nama_produk' => 'required|string|max:255',
-    //         'harga_beli' => 'required|numeric|min:0',
-    //         'produk_satuan' => 'required|array',
-    //         'produk_satuan.*.satuan' => 'required|string|max:50',
-    //         'produk_satuan.*.harga_jual_eceran' => 'required|numeric|min:0',
-    //         'produk_satuan.*.harga_jual_borongan' => 'nullable|numeric|min:0',
-    //     ]);
-
-    //     $data = $request->all();
-
-    //     if (isset($data['produk_satuan'])) {
-    //         foreach ($data['produk_satuan'] as $satuan) {
-    //             if (!SatuanProduk::where('nama', $satuan['satuan'])->exists()) {
-    //                 SatuanProduk::create(['nama' => $satuan['satuan']]);
-    //             }
-    //         }
-    //     }
-
-    //     // Simpan data produk
-    //     $produk = Produk::create([
-    //         'kode_produk' => $request->kode_produk,
-    //         'nama_produk' => $request->nama_produk,
-    //         'id_kategori' => $request->id_kategori,
-    //         'merk' => $request->merk,
-    //         'harga_beli' => $request->harga_beli,
-    //         'diskon' => $request->diskon,
-    //         'stok' => $request->stok,
-    //     ]);
-
-    //     // Simpan data produk_satuan
-    //     foreach ($request->produk_satuan as $satuan) {
-    //         ProdukSatuan::create([
-    //             'id_produk' => $produk->id_produk,
-    //             'satuan' => $satuan['satuan'],
-    //             'harga_jual_eceran' => $satuan['harga_jual_eceran'],
-    //             'harga_jual_borongan' => $satuan['harga_jual_borongan'],
-    //         ]);
-    //     }
-
-    //     return response()->json('Data berhasil disimpan', 200);
-    // }
-
     public function show($id)
     {
         $produk = Produk::with('satuan')->find($id);
@@ -255,69 +203,6 @@ class ProdukController extends Controller
         return response()->json('Data berhasil diperbarui', 200);
     }
 
-
-    // public function update(Request $request, $id)
-    // {
-    //     // Validasi input
-    //     $request->validate([
-    //         'kode_produk' => "required|unique:produk,kode_produk,{$id},id_produk",
-    //         'nama_produk' => 'required|string|max:255',
-    //         'harga_beli' => 'required|numeric|min:0',
-    //         'produk_satuan' => 'required|array',
-    //         'produk_satuan.*.satuan' => 'required|string|max:50',
-    //         'produk_satuan.*.harga_jual_eceran' => 'required|numeric|min:0',
-    //         'produk_satuan.*.harga_jual_borongan' => 'required|numeric|min:0',
-    //     ]);
-    //     $produk = Produk::with('produkSatuan')->find($id);
-
-    //     $produk->update($request->except('produk_satuan'));
-
-    //     // Update data produk_satuan
-    //     $dataBaru = $request->produk_satuan;
-
-    //     // Ambil data lama
-    //     $dataLama = $produk->produkSatuan->keyBy('satuan')->toArray();
-
-    //     $data = $request->all();
-
-    //     if (isset($data['produk_satuan'])) {
-    //         foreach ($data['produk_satuan'] as $satuan) {
-    //             if (!SatuanProduk::where('nama', $satuan['satuan'])->exists()) {
-    //                 SatuanProduk::create(['nama' => $satuan['satuan']]);
-    //             }
-    //         }
-    //     }
-
-    //     // Loop data baru untuk memperbarui atau menambah
-    //     foreach ($dataBaru as $satuanBaru) {
-    //         if (isset($dataLama[$satuanBaru['satuan']])) {
-    //             // Jika sudah ada, update
-    //             ProdukSatuan::where('id_produk', $produk->id_produk)
-    //                 ->where('satuan', $satuanBaru['satuan'])
-    //                 ->update(['harga_jual_eceran' => $satuanBaru['harga_jual_eceran'], 'harga_jual_borongan' => $satuanBaru['harga_jual_borongan']]);
-
-    //             // Hapus dari data lama (sudah diupdate)
-    //             unset($dataLama[$satuanBaru['satuan']]);
-    //         } else {
-    //             // Jika belum ada, tambahkan
-    //             ProdukSatuan::create([
-    //                 'id_produk' => $produk->id_produk,
-    //                 'satuan' => $satuanBaru['satuan'],
-    //                 'harga_jual_eceran' => $satuanBaru['harga_jual_eceran'],
-    //                 'harga_jual_borongan' => $satuanBaru['harga_jual_borongan'],
-    //             ]);
-    //         }
-    //     }
-
-    //     // Hapus data lama yang tidak ada di data baru
-    //     if (count($dataLama) > 0) {
-    //         ProdukSatuan::where('id_produk', $produk->id_produk)
-    //             ->whereIn('satuan', array_keys($dataLama))
-    //             ->delete();
-    //     }
-
-    //     return response()->json('Data berhasil diperbarui', 200);
-    // }
 
     public function destroy($id)
     {
