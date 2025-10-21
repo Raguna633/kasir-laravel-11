19320ba2f206194b98e46551cde54a21e3a938af
Raguna633 <88103737+Raguna633@users.noreply.github.com>
2025-10-21 15:05:59 +0700

    ref: change data type for jumlah from int to decimal

--- database/migrations/2021_03_05_200858_buat_penjualan_detail_table.php
+++ database/migrations/2021_03_05_200858_buat_penjualan_detail_table.php
@@ -19,7 +19,7 @@ class BuatPenjualanDetailTable extends Migration
             $table->integer('id_produk');
             $table->integer('harga_jual_eceran')->nullable();
             $table->integer('harga_jual_borongan')->nullable();
-            $table->integer('jumlah');
+            $table->decimal('jumlah', 10, 2);
             $table->tinyInteger('diskon')->default(0);
             $table->integer('subtotal');
             $table->boolean('status')->default(0);
